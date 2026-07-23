4941b3d0fdfe6011b56c6c15fc30f90aa9bf8ed0
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2026-07-23 11:10:36 +0000

    change

--- new.cpp
+++ new.cpp
@@ -4,6 +4,8 @@ int main() {
     std::cout << "Whats up" << std::endl;
     int a = 128;
 
+    double x1 = 33.3;
+    double x2 = 44.4;
     
     long b = 10002343456;
     std::cout << a / b << std::endl;
